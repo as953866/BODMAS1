5025591d987a681ab9a22de91deed293b3240e56
as953866 <64868585+as953866@users.noreply.github.com>
2020-05-13 11:36:38 +1200

    Add project files.

---
+++ BODMAS1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BODMAS1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            label1.Text = (num1 / num2).ToString();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox3.Text);
+            int num2 = Convert.ToInt32(textBox4.Text);
+            label1.Text = (num1 * num2).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox5.Text);
+            int num2 = Convert.ToInt32(textBox6.Text);
+            label1.Text = (num1 + num2).ToString();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox7.Text);
+            int num2 = Convert.ToInt32(textBox8.Text);
+            label1.Text = (num1 - num2).ToString();
+        }
+    }
+}
